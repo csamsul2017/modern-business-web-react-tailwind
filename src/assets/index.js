88d9ec3cbe22d18adb3029afdeebb65d057aea01
csamsul2017 <csamsul2017@gmail.com>
2025-08-13 06:24:03 +0700

    company logo with animation

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import slack from "./slack.png";
+import amazon from "./amazon.png";
+import woocommerce from "./woocommerce.png";
+import meundies from "./meundies.png";
+import sitepoint from "./sitepoint.png";
+
+export { slack, amazon, woocommerce, meundies, sitepoint };
